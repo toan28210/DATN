2d1c302db836e095578ae40ca99fdbaa41b4944e
toan28210 <thaitoan28210@gmail.com>
2022-11-07 12:04:56 +0700

    Api

---
+++ Regression/test.py
@@ -0,0 +1,9 @@
+import numpy as np
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import r2_score
+import matplotlib.pyplot as plt
+import seaborn as sns
+import warnings
+warnings.filterwarnings("ignore")
